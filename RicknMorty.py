e79d7d414de3b1ab17856ab7e8f911f9fe5e83c1
anelenzamarsa <41071616+anelenzamarsa@users.noreply.github.com>
2023-12-19 20:30:05 +0100

    Update RicknMortyAPI.py

--- RicknMorty.py
+++ RicknMorty.py
@@ -2,82 +2,68 @@ import requests
 import csv
 from tabulate import tabulate
 
+API_BASE_URL = "https://rickandmortyapi.com/api/character/"
 
 
-
-def initialRequest(name):
-    query = requests.get(url=f"https://rickandmortyapi.com/api/character/?name={name}")
+def initial_request(name):
+    # Make the initial request to the Rick and Morty API with the provided character name
+    params = {'name': name}
+    query = requests.get(API_BASE_URL, params=params)
     return query.json()
 
 
-
-def showResults(output):
-    next = output['info']['next']
-    saveToFile(request_output=output) ## save to file the initial request
-    page = 2
-
-    while next is not None: # pagination
-        sub_query = requests.get(url=next) #do API call for the rest of pages
-        sub_output = sub_query.json()
-        saveToFile(request_output=sub_query.json())
-
-        next = sub_output['info']['next']
-        page += 1
-
-    with open('data.csv', newline='') as csvfile:
-        csvreader = csv.reader(csvfile)
-        data = list(csvreader)
-
-    # Print the CSV data in a tabular format
-    print(tabulate(data, headers='firstrow', tablefmt='grid'))
-
-
-
-def getEpisode(endpoint):
-    call = requests.get(endpoint)
-    result = call.json()
+def get_episode(endpoint):
+    # Make a request to the provided endpoint and format the episode information
+    result = requests.get(endpoint).json()
     return f"{result['name']}-({result['episode']})"
 
 
+def save_to_file(request_output):
+    # Extract character information from the API response and save it to a CSV file
+    results = request_output['results']
 
-
-
-
-def saveToFile(request_output):
-    results = request_output['results'] # all characters in list as list
-
-
-    with open('data.csv', mode='a', newline='') as character_file: ## populating table
+    with open('data.csv', mode='a', newline='') as character_file:
         writer = csv.writer(character_file)
 
-
         for character in results:
-
-            type = "-"
-            if character['type'] != "":
-                type = character['type']  ## in case type is None just add - to have a better seperation in lines
-
-            episodes = ", ".join([ getEpisode(e) for e in character['episode'] ]) ## Join in String All episode name-shortcut
-
-            row = [character['id'], character['name'], character['status'], character['species'], type, character['gender'], character['origin']['name'],character['location']['name'], episodes ] # row to be inserted
+            # Set 'type' to '-' if it's empty
+            type_value = character.get('type', '-')
+            # Join episode names and shortcuts into a string
+            episodes = ", ".join([get_episode(e) for e in character['episode']])
+            # Create a row to be inserted into the CSV file
+            row = [character.get(key, '-') for key in ['id', 'name', 'status', 'species', 'type', 'gender']] + \
+                  [character['origin']['name'], character['location']['name'], episodes]
             writer.writerow(row)
 
 
-
 def workflow(name):
+    # Create and truncate the CSV file, write column headers
     with open('data.csv', mode='w') as file:
-        file.flush() ## Truncating table
-        column_names = ['ID', 'Name', 'Status', 'Species', 'Type', 'Gender', 'Origin', 'Location', 'Episodes'] # write headers
-        writer = csv.writer(file)
-        writer.writerow(column_names)
+        csv.writer(file).writerow(['ID', 'Name', 'Status', 'Species', 'Type', 'Gender', 'Origin', 'Location', 'Episodes'])
 
+    response = initial_request(name=name)
 
-    showResults(initialRequest(name=name))
+    if 'error' in response:
+        print(f"Error: {response['error']}")
+    else:
+        save_to_file(request_output=response)
+        next_page = response['info']['next']
 
+        while next_page:
+            sub_output = requests.get(next_page).json()
 
+            if 'error' in sub_output:
+                print(f"Error: {sub_output['error']}")
+                break
+
+            save_to_file(request_output=sub_output)
+            next_page = sub_output['info']['next']
+
+        with open('data.csv', newline='') as csvfile:
+            data = list(csv.reader(csvfile))
+            print(tabulate(data, headers='firstrow', tablefmt='grid'))
 
 
 if __name__ == '__main__':
-    param = input("Let's find your Rick n Morty Character!!!\n" + "Please enter the characters name: ")
-    print(workflow(param))
-
+    param = input("Let's find your Rick n Morty Character!!!\nPlease enter the character's name: ")
+    workflow(param)
